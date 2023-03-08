72584726c6bcc85c50c17a861cd58bae631878cd
iiPython <ben@iipython.cf>
2023-03-07 22:26:46 -0600

    Actual mathematics

--- chungus.py
+++ chungus.py
@@ -1,20 +1,41 @@
 # Copyright 2023 iiPython
 
-# Base classes
-class TokenTypes:
-    TOK_ADD = "+"
-    TOK_SUB = "-"
-    TOK_MUL = "*"
-    TOK_DIV = "/"
-    TOK_POW = "^"
+# Modules
+from types import FunctionType
+from typing import List
 
+# Base classes
+class Node(object):
+    def __init__(self, lexical: str, calculate: FunctionType) -> None:
+        self.lexical = lexical
+        self.calculate = calculate
+
+    def __str__(self) -> str:
+        return self.lexical
+
+TT_TYPES = {
+    n.lexical: n
+    for n in [
+        Node("+", lambda x, y: x + y),
+        Node("-", lambda x, y: x - y),
+        Node("*", lambda x, y: x * y),
+        Node("/", lambda x, y: x / y),
+        Node("^", lambda x, y: x ** y)
+    ]
+}
+
+# Operation class
 class Operation(object):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, left: int | float, right: int | float, node: Node) -> None:
+        self.value = node.calculate(
+            left if not isinstance(left, Operation) else left.value,
+            right if not isinstance(right, Operation) else right.value
+        )
 
 # Parser
 def parse(expression: str) -> List[Operation]:
-    return "debug"
+    return "nothing yet"
 
 # Testing
 print(parse("2*(3^(2-5))"))
+print(Operation(5, 6, TT_TYPES["-"]).value)
